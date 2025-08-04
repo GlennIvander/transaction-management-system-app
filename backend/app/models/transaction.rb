16a5ba4060e70f4e4ac8274e5681535e1682096c
GlennIvander <ivander792@gmail.com>
2025-08-04 16:50:55 +0800

    for step-by-step instruction

--- backend/app/models/transaction.rb
+++ backend/app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ApplicationRecord
+  validates :account_number, presence: true
+  validates :account_holder, presence: true
+  validates :amount, presence: true
+  validates :transaction_date, presence: true
 end
